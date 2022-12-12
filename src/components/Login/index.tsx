6234aa6686da3298671d7d06030544c18edc7a43
AmiltonCabral <amilton.cabral@splab.ufcg.edu.br>
2022-12-12 09:02:49 -0300

    Feat: login style

--- src/components/Login/index.tsx
+++
@@ -1,5 +0,0 @@
-function Login() {
-  return <h2>Sou login</h2>
-}
-
-export default Login
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head'
-import Login from '../components/Login'
+import Button from '../components/Button'
+import Input from '../components/Input'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.fsEntitiesGallery}>
       <Head>
         <title>Gerenciamento de estoque</title>
         <meta
@@ -14,7 +15,26 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Login />
+      <div data-fs-menu>
+        <h2>Gerenciamento de estoque</h2>
+
+        <form data-fs-form>
+          <div>
+            <label data-fs-form-label>
+              Login
+              <Input type="text" />
+            </label>
+            <label data-fs-form-label>
+              Senha
+              <Input type="password" />
+            </label>
+          </div>
+
+          <div data-fs-buttons>
+            <Button>Entrar</Button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
